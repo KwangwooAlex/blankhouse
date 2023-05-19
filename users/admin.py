c6ec4387dc921bda34e6e02140b96307e63a1e38
Kwangwoo Back <bnc3049@gmail.com>
2023-05-18 19:09:14 -0600

    First commit: :

---
+++ users/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+# 유저 model을 등록시킬것임
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "username",
+                    "password",
+                    "email",
+                    "is_host",
+                    "emergency_contact",
+                    "gender",
+                    "language",
+                    "currency",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                ),
+                # 처음에 보여줄지안보여줄지
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_display = ("pk", "username", "email", "is_host", "is_superuser")
---
+++ users/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .validators import validate_file_size
+
+
+# AbstractUser 의 경로에 있는 User class말고 우리가 커스텀 한걸 쓰기위해서는
+# setting.py에서 다른 경로를 설정 해주어야한다
+class User(AbstractUser):
+    # 오버라이딩을 하여 성, 이름을 사용하지않고 하나의 name 이라는 새로운 필드를 커스텀한다(상속에서 오버라이딩)
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")  # 첫번쨰는 디비에 들어갈 value 두번째는 admin패널에 보일 내용
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    password = models.CharField(max_length=128)
+    avatar = models.FileField(
+        upload_to="media/", validators=[validate_file_size], blank=True, null=True
+    )
+    email = models.CharField(max_length=150, blank=True, null=True)
+    balance = models.PositiveIntegerField(default=0)
+    address = models.CharField(max_length=150, blank=True, null=True)
+    emergency_contact = models.CharField(max_length=11, blank=True, null=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
+
+    is_host = models.BooleanField(default=False)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
